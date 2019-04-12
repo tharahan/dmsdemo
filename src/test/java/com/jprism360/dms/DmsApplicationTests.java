90b9fe23d2cdba4d798836399ebe85c08eba27c9
Tharahan Muthu <tharahan@gmail.com>
2019-04-12 18:11:47 -0400

    initial skeleton project

---
+++ src/test/java/com/jprism360/dms/DmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jprism360.dms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
